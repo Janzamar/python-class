3089586b0d0c90edd768b36e38d2ff6bc413ccd6
Janzamar <jazamar@lcg.unam.mx>
2025-04-25 12:44:07 -0600

    Creación de "at_content.py"

---
+++ Block de notas/at_content.py
@@ -0,0 +1,12 @@
+def at_content(dna):
+
+    dna = dna.upper()
+    length = len(dna)
+    a_count = dna.count("A")
+    t_count = dna.count("T")
+    at_content = (a_count + t_count)/length
+    print(at_content)
+    
+
+at_content("GATGCTAGCTAGCTGATCGATGCTAGCTAGCCCTAGCAA")
+at_content("AGTGTGATGATGTACACACACAGTTAGAGTGA")
